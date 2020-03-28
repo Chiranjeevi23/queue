e5b231257ccb7d3222f9f0738cebd1ebf8e24585
chiranjeevi <nchiru987@gmail.com>
2020-03-28 22:18:06 +0530

    intial commit

---
+++ User.java
@@ -0,0 +1,29 @@
+package hii;
+
+public class User {
+	 int na;
+	 int size;
+	 int front;
+	 int rear;
+	 
+	 
+	int w[] = new int [5];
+	public int qu(int data){
+	    w[na] = data;
+		 na = na+1;
+		 size +=1;
+		 return data + 55;
+		 
+	}
+	public void show(){
+		for (int i=0; i< size;i++){
+			System.out.println(w[i]);	
+		}
+	}
+	public void sh(int ka){
+		System.out.println(ka+55);
+	}
+}
+
+
+
